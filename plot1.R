35136ff8a3420584224f26e21f7babb5a75b4c9f
vishagan1 <svishagan@gmail.com>
2016-05-15 15:44:44 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 library(sqldf)
 fileh <- file("household_power_consumption.txt")
-df <- df<-sqldf("select * from fileh where Date <= '2/2/2007' and Date >= '1/2/2007'", file.format = list(header = TRUE, sep = ";"))
+df<-sqldf("select * from fileh where Date == '2/2/2007' and Date == '1/2/2007'", file.format = list(header = TRUE, sep = ";"))
 
 png("plot1.png",width = 480, height = 480)
 hist(df$Global_active_power,xlab = "Global Active Power (kilowatts)", col = 'red', main = "Global Active power")
